c78720283777361275af30cf848854cebf3113ab
tahar-irki <taharirki8@gmail.com>
2025-05-02 22:36:59 +0100

    added NotFoundPage

--- frontend/pfe-project/src/router/index.js
+++ frontend/pfe-project/src/router/index.js
@@ -14,6 +14,7 @@ import makeReservation from '@/views/makeReservation.vue'
 import OwnerPage from '@/views/ownerPage.vue'
 import OuReservations from '@/views/ouReservations.vue'
 import OuReviews from '@/views/ouReviews.vue'
+import NotFoundPage from '@/views/notFoundPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -93,6 +94,11 @@ const router = createRouter({
       path:'/makeReservation',
       name:'makeReservation',
       component:makeReservation
+    },
+    {
+      path:'/:catchAll(.*)',
+      name:'Not-Found-Page',
+      component:NotFoundPage
     }
   ],
 })
---
+++ frontend/pfe-project/src/views/notFoundPage.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+
+<template>
+
+
+<section class="text-center flex flex-col justify-center items-center h-96">
+      <i class="pi pi-exclamation-triangle text-yellow-400 mb-5" style="font-size: 6rem"></i>
+      <h1 class="text-6xl font-bold mb-4">404 Not Found</h1>
+      <p class="text-xl mb-5">This page does not exist</p>
+      <RouterLink
+        to="/"
+        class="text-white bg-orange-700 hover:bg-orange-900 rounded-md px-3 py-2 mt-4"
+        >Go Back
+    </RouterLink>
+    </section>
+</template>
